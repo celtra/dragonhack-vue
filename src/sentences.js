26c5d639732e73a3f55ad385a219ad1eeddacfba
Matevz Stinjek <matevz.stinjek@celtra.com>
2018-05-16 14:59:09 +0200

    Actual meaningful thought out sentences

--- src/sentences.js
+++ src/sentences.js
@@ -1,4 +1,5 @@
 export default [
-    'Some sample text you have to type out really quickly to win',
-    'Another sentence'
+    'Some sample text you have to type out really quickly to win if you want to be the very best, like no one ever was.',
+    'To catch them all is your real quest, to train them is your cause',
+    'Yu gi oh!'
 ]
