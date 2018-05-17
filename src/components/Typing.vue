685fb4dee06c3dee09bc83a5e27fa5e226e8b176
Matevz Stinjek <matevz.stinjek@celtra.com>
2018-05-17 11:31:26 +0200

    made it better

--- src/components/Typing.vue
+++ src/components/Typing.vue
@@ -4,12 +4,12 @@
             <p v-if="streakText">{{ streakText }}</p>
         </div>
 
-        <div class="sentence" tabindex="0" @keydown="handleInput" ref="text">
-            <span v-for="(c, index) in text" :key="c.id" :class="getClass(index)">{{ c }}</span>
+        <div class="sentence" tabindex="0" @keydown="handleInput" ref="sentence">
+            <span v-for="(c, index) in sentence" :key="c.id" :class="getClass(index)">{{ c }}</span>
         </div>
 
         <p class="time">{{ Math.floor(time) }}s</p>
-        <p class="score">{{ correctIndices.length }} / {{ written.length }}</p>
+        <p class="score">{{ correctCharsCount }} / {{ text.length }}</p>
         <p class="streak">{{ correctIndicesInLastSeconds.length }} characters in the last 5 seconds</p>
 
         <div class="performance">
@@ -26,42 +26,40 @@ export default {
         return {
             streakDuration: 3,
             sentenceIndex: 0,
-            written: '',
-            correctCount: 0,
+            text: '',
+            totalCorrectCount: 0,
             time: 0,
             writeTimes: [],
             timeResults: []
         }
     },
     mounted () {
-        this.$refs.text.focus()
+        this.$refs.sentence.focus()
 
         this.startTime = Date.now() / 1000
 
         this.intervalId = setInterval(() => {
             this.time = Date.now() / 1000 - this.startTime
             this.timeResults.push(this.correctIndicesInLastSeconds.length)
-            this.$refs.text.focus()
+            this.$refs.sentence.focus()
         }, 1000)
     },
     beforeDestroy () {
         clearInterval(this.intervalId)
     },
     computed: {
-        text () {
+        sentence () {
             return sentences[this.sentenceIndex]
         },
-        correctIndices () {
-            let indices = []
-            Array.from(this.written).forEach((c, index) => {
-                if (c === this.text[index])
-                    indices.push(index)
-            })
-            return indices
+        textResult () {
+            return Array.from(this.text).map((c, index) => c === this.sentence.charAt(index))
+        },
+        correctCharsCount () {
+            return this.textResult.filter(c => c).length
         },
         correctIndicesInLastSeconds () {
             return this.writeTimes.filter((time, index) =>  
-                time > this.time - this.streakDuration && this.correctIndices.includes(index))
+                time > this.time - this.streakDuration && this.textResult[index])
         },
         streakText () {
             if (this.correctIndicesInLastSeconds.length >= 8 * this.streakDuration)
@@ -80,32 +78,36 @@ export default {
         },
         deleteInput () {
             this.writeTimes = this.writeTimes.slice(0, -1)
-            this.written = this.written.slice(0, -1)
+            this.text = this.text.slice(0, -1)
         },
         addInput (key) {
             this.writeTimes.push(Date.now() / 1000 - this.startTime)
-            this.written += key
+            this.text += key
 
-            if (this.written.length === this.text.length)
+            if (this.text.length === this.sentence.length)
                 this.handleEndOfSentence()
         },
         handleEndOfSentence () {
-            this.correctCount += this.correctIndices.length
-            this.written = ''
+            this.totalCorrectCount += this.correctCharsCount
+            this.text = ''
+            this.writeTimes = []
 
             if (this.sentenceIndex === sentences.length - 1) {
                 let time = Date.now() / 1000 - this.startTime
-                this.$emit('score', this.correctCount / time)
+                this.$emit('score', this.totalCorrectCount / time)
             } else {
                 this.sentenceIndex ++
             }
         },
         getClass (index) {
-            return {
-                current: index === this.written.length,
-                correct: this.correctIndices.includes(index),
-                wrong: index < this.written.length && !this.correctIndices.includes(index)
-            }
+            if (index > this.text.length)
+                return ''
+            else if (index === this.text.length)
+                return 'current'
+            else if (this.textResult[index])
+                return 'correct'
+            else
+                return 'wrong'
         }
     }
 }
