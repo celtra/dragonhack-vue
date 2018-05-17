728cee51be1aab1e7ea2674736e4497ef51443b9
zan <zan.kusterle@gmail.com>
2018-05-17 13:33:12 +0200

    fix data

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const sentences =  [
     'Some sample text you have to type out really quickly to win if you want to be the very best, like no one ever was.',
-    'To catch them all is your real quest, to train them is your cause',
+    'To catch them all is your real quest, to train them is your cause,',
     'Yu gi oh!'
 ]
 
