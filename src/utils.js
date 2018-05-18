5bd4c0f7b26fb8c21ddc7ab424d7daa456c3f141
zan <zan.kusterle@gmail.com>
2018-05-18 20:55:56 +0200

    utils cleanup

--- src/utils.js
+++ src/utils.js
@@ -7,28 +7,14 @@ export const sentences = [
 
 export function listenForInput(addCb, removeCb) {
     window.addEventListener('keydown', (e) => {
-        e.preventDefault()
-        e.stopPropagation()
-
         if (e.key === 'Backspace') {
+            e.preventDefault()
+            e.stopPropagation()
+
             if (removeCb)
                 removeCb()
         } else if (e.key.length === 1 && e.key.charCodeAt(0) >= 32 && e.key.charCodeAt(0) <= 126) {
             addCb(e.key)
         }
     })
-}
-
-export function getRange(stringList, globalIndex) {
-    let position = 0
-    for (let string of stringList) {
-        position += string.length
-        if (globalIndex < position) {
-            return {
-                from: position - string.length,
-                to: position
-            }
-        }
-    }
-    return null
 }
